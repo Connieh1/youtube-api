ea86340e9834a32408fe46211e3c53ed1d28a943
Connieh1 <connie2630a@gmail.com>
2021-01-30 20:46:27 -0800

    Build search and fetch function in app.js

--- app.js
+++ app.js
@@ -1,10 +1,19 @@
 const api_key = config.API_KEY;
+const btn = document.querySelector("button");
+btn.addEventListener("click", ySearch);
 
 function ySearch(e) {
+  const searchTerm = document.querySelector("input");
+  searchTerm.setAttribute("value", "test");
+  let search = searchTerm.value;
+  search = encodeURIComponent(search);
   const url =
     "https://www.googleapis.com/youtube/v3/search/?part=snippet&key=" +
     api_key +
-    "&q=test&maxResults=20";
+    "&q=" +
+    search +
+    "&maxResults=20";
+  const output = document.querySelector(".output");
   document.querySelector(".output").textContent = url;
   fetch(url)
     .then((resp) => resp.json())
